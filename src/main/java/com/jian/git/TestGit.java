6dac48da7e7e3b1fb00fc872958f8239d46c4d89
Json <Json@qq.com>
2022-04-26 16:25:08 +0800

    gitee push

--- src/main/java/com/jian/git/TestGit.java
+++ src/main/java/com/jian/git/TestGit.java
@@ -10,5 +10,6 @@ public class TestGit {
         System.out.println("hot-fix commit");
         System.out.println("push commit");
         System.out.println("pull commit");
+        System.out.println("Gitee push");
     }
 }
